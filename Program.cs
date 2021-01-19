04a000141799faa0ea2d990b61f0945128a8f4a8
ioanamiruna <ioana.miruna.ib@gmail.com>
2021-01-19 18:48:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            int winNum = r.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0 and 100");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("WINNER");
+                    win = true;
+                }
+            }
+            while (win == false);
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+        }
+    }
+}
